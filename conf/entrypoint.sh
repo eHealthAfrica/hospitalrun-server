3337442005816571e5b7da9e309a8d2326826e76
Mofesola Babalola <me@mofesola.com>
2017-03-07 15:21:52 +0100

    Docker config and ES env variables

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-RUN service atd start
-RUN at -f /usr/src/app/conf/initcouch.sh now + 1 min
-RUN echo "/usr/src/app/utils/elasticsearch.sh hradmin test" | at now + 1 min
+service atd start
+at -f /usr/src/app/conf/initcouch.sh now + 1 min
 npm start
