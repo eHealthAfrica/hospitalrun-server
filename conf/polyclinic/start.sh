a2898d69763dc42a26390c9051a64704927d638b
Mofesola <me@mofesola.com>
2017-03-22 14:45:48 +0100

    Modify start.sh to use relative docker-compose file

--- conf/polyclinic/start.sh
+++ conf/polyclinic/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker rm $(sudo docker ps -a -q) || : && cd ~/polyclinic && sudo docker-compose -f ~/polyclinic/docker-compose-polyclinic.yml up --build -d
+sudo docker rm $(sudo docker ps -a -q) || : && cd ~/polyclinic && sudo docker-compose -f docker-compose-polyclinic.yml up --build -d
